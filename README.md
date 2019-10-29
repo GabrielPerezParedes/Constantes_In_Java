dbe0d10d4f568492f975cedc18ba13d255c117ae
Gabriel Perez Paredes <gabogabygpp16@gmail.com>
2019-10-28 23:13:01 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Constantes_In_Java
---
+++ src/view/Print.java
@@ -0,0 +1,18 @@
+	package view;
+	
+	public class Print {
+		
+		public static void main(String[] args) {
+		
+		final int numero = 10;
+		/*Cuando ponemos la palabra "final por delante de la variable" esta variable 
+		 * sera constante de principio a fin aunque la intentemos cambiar de valor 
+		 * como vemos a continuacion
+		 */
+		
+		//numero = 15;
+
+		System.out.println("El numero impreso es:" +numero);
+	
+		}
+	}
